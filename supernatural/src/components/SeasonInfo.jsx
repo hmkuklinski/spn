2ddef32761c56e36d83e25198ef1673037449066
hmkuklinski <hmkuklinski@gmail.com>
2025-06-05 04:03:47 -0400

    fixed episodeInfo.js to be more readable, added individual episode objects. formatting for the season summary pages, with spoiler unlock buttons
    need to enter remaining information for episodes and finish each season info. add bodycount values for each major character death.

--- supernatural/src/components/SeasonInfo.jsx
+++ supernatural/src/components/SeasonInfo.jsx
@@ -1,54 +1,67 @@
 //takes title, imgsrc, description, majorUpdates, characterIntros, epNumbers, yearAired, totalMonsters, bodyCount
 import { episodes } from "./episodeInfo";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 export default function SeasonInfo({num}){
     const myEp = episodes[num];
-    const [spoilers, setSpoilers] = useState(false);
-    const showDeaths=()=>{
-        setSpoilers(true);
-    }
+    const [showDeaths, setShowDeaths] = useState(false);
+    const [showUpdates, setShowUpdates] = useState(false);
+
+    useEffect(()=>{
+        setShowDeaths(false);
+        setShowUpdates(false);
+    }, [num]);
     return (
         <div className="pages">
             <div className="left-page">
-                <div className="season-info">
+                <div className="season-title">
                     <h2>{myEp.title}</h2>
                 </div>
-                {Array.isArray(myEp.majorUpdates) && (
-                    <div className="char-updates">
-                        {myEp.majorUpdates.map((update) => (
-                            <div className="update-container" key={update.id}>
-                                <div className="char-name"><h3>{update.id}</h3></div>
-                                <div className="update">{update.text}</div>
-                            </div>
-                        ))}
-
-                    </div>
-                )}
+                <div className="season-info">
+                    <p>{myEp.description}</p>
+                </div>
                 {Array.isArray(myEp.characterIntros) && (
-                     <div className="char-intros">
-                        {myEp.characterIntros.map((intro)=>(
-                            <div className="intro-container">
-                                <div className="char-name"><h3>{intro.id}</h3></div>
-                                <div className="update">{intro.text}</div>
-                            </div>
-                        ))}
+                <div className="char-section">
+                    <div className="char-title">
+                    <h3>New Characters Introduced</h3>
                     </div>
-                )}
-                {Array.isArray(myEp.majorDeaths) && (
-                    <div className="char-deaths">
-                        <div className="" id="btn-death">
-                            {!spoilers && <button onClick={showDeaths()}>Unlock Lore</button>}
-                            {spoilers &&
-                                Array.isArray(myEp.majorDeaths) &&
-                                myEp.majorDeaths.map((char) => (
-                                    <div className="characters" key={char.id}>
-                                        <p>{char.id}: {char.text}</p>
-                                    </div>
-                                ))
-                            }
+                    {myEp.characterIntros.map((intro) => (
+                    <div className="char-entry" key={intro.id}>
+                        <div className="char-name">
+                        <Link to={`/characters/${intro.id}`}><h3>{intro.name}</h3></Link>
                         </div>
+                        <div className="char-text">{intro.text}</div>
                     </div>
+                    ))}
+                </div>
                 )}
+
+                {Array.isArray(myEp.majorUpdates) && (
+                <div className="char-section" id="intros">
+                    <div className="char-title">
+                        <h3>Major Character Updates</h3>
+                    </div>
+                    {!showUpdates && <div className="updates"><button onClick={() => setShowUpdates(true)}>Unlock Spoilers</button></div>}
+                    {showUpdates && (
+                    <>
+                        
+                        {myEp.majorUpdates.map((update) => (
+                        <div className="char-entry" key={update.id}>
+                            <div className="char-name">
+                            <Link to={`/characters/${update.id}`}>
+                                <h3>{update.name}</h3>
+                            </Link>
+                            </div>
+                            <div className="char-text">{update.text}</div>
+                        </div>
+                        ))}
+                    </>
+                    )}
+                </div>
+                )}
+
+                
+
             </div>
             <div className="right-page">
                 <div className="season-gif">
@@ -57,21 +70,43 @@ export default function SeasonInfo({num}){
                 <div className="info-section">
                     <div className="info">
                         <div className="info-title"><h4>Episodes </h4></div>
-                        <div className="info-answer">{myEp.episodes}</div>
+                        <div className="info-answer">{myEp.epNumbers}</div>
                     </div>
                     <div className="info">
                         <div className="info-title"><h4>Year Aired </h4></div>
                         <div className="info-answer">{myEp.yearAired}</div>
                     </div>
                     <div className="info">
-                        <div className="info-title"><h4>Total Monsters </h4></div>
-                        <div className="info-answer">{myEp.totalMonsters}</div>
+                        <div className="info-title"><h4>IMDb Rating</h4></div>
+                        <div className="info-answer">{myEp.ratings}</div>
                     </div>
                     <div className="info">
-                        <div className="info-title"><h4>Body Count</h4></div>
+                        <div className="info-title"><h4>Major Deaths</h4></div>
                         <div className="info-answer">{myEp.bodyCount}</div>
                     </div>
                 </div>
+                {Array.isArray(myEp.majorDeaths) && (
+                <div className="char-section" id="deaths">
+                    <div className="char-title">
+                        <h3>Major Deaths</h3>
+                    </div>
+                    {!showDeaths && <div className="spoilers"><button onClick={() => setShowDeaths(true)}>Unlock Spoilers</button></div>}
+                    {showDeaths && (
+                    <>
+                        
+                        {myEp.majorDeaths.map((char) => (
+                        <div className="char-entry" key={char.id}>
+                            <div className="char-name">
+                            <Link to={`/characters/${char.id}`}>
+                                <h3>{char.name}{char.text ? `: ${char.text}` : ''}</h3>
+                            </Link>
+                            </div>
+                        </div>
+                        ))}
+                    </>
+                    )}
+                </div>
+                )}
             </div>
         </div>
     );
