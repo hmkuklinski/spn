b35993b12555fb311bc8bb59f5976c9e6a940e4f
unknown <hmkuklinski@gmail.com>
2026-03-06 22:57:12 -0500

    edits to get_aired --> now shows get_aired endpoint, not get_writers

--- supernatural/pages/view_api/get_aired.js
+++ supernatural/pages/view_api/get_aired.js
@@ -1,7 +1,7 @@
 import EpisodeAPI from "../../src/components/api_results/EpisodeAPI";
 
 export default function ViewAirDate(){
-    const index= 17;
+    const index= 18;
     const fetchUrl = `https://supernatural-lore.vercel.app/api/episodeListAPI?seasonNum=1&epNum=1&type=get_aired`;
     const example_data = ("9/13/2005");
     return <EpisodeAPI fetchUrl={fetchUrl} example_data={example_data} indexId={index}/>;
