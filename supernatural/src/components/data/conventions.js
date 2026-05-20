077e30d8d7d1432dc6fd2024475cd0db8806575b
unknown <hmkuklinski@gmail.com>
2026-05-20 17:51:51 -0400

    updates to conventions page. added 2027 and back half of 2026.
    todo: edit guests

--- supernatural/src/components/data/conventions.js
+++ supernatural/src/components/data/conventions.js
@@ -1,30 +1,207 @@
 // {id:"", location:"", eventLocation:"", startMonth:"", startDate:"", startYear:"", guests:""},
 export const conventions= [
+  {
+    "id":"convention4",
+    "name": "The Road So Far... The Road Ahead. Creation Tour '27",
+    "desc":"Over 20 years ago we invited a young actor to one of our horror conventions, and he was a smash hit with our audience — Jared Padalecki. What can we say? The rest became history! We are simply amazed that it’s been 20 years of celebrating the long-running hit series Supernatural, presenting conventions with the show’s superstar headliners Jensen Ackles, Jared Padalecki and Misha Collins!",
+    "imgsrc":"/assets/beyond/con27.png",
+    "conventionList": [
+        {
+        "id": "saltlake4",
+        "location": "Salt Lake City, UT",
+        "eventLocation": "Hyatt Regency Salt Lake City Hotel",
+        "startMonth": "JAN",
+        "startDate": "15",
+        "startYear": "2027",
+        "guests": "Jensen Ackles, Misha Collins, Rob Benedict, Richard Speight Jr., Felicia Day, Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"
+      },
+        {
+        "id": "arlington4",
+        "location": "Arlington, VA",
+        "eventLocation": "Crystal Gateway Marriott Hotel",
+        "startMonth": "MAR",
+        "startDate": "19",
+        "startYear": "2027",
+        "guests": "Jensen Ackles, Misha Collins, Rob Benedict, Richard Speight Jr., Felicia Day, Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"
+      },
+
+    ]
+  },
+  {
+    "id": "convention3",
+    "name": "The Road So Far... The Road Ahead. Creation Tour",
+    "desc": "We are ready for 2026 with several incredible in-person engagements with superstars Jensen Ackles and Jared Padalecki. We celebrate the adventures of two brothers who set of on a journey to save people and hunt things. Make your plans to be with us and check the calendar below for dates!",
+    "imgsrc": "/assets/beyond/rsf.png",
+    "conventionList": [
+      {
+        "id": "vegas3",
+        "location": "Las Vegas, NV",
+        "eventLocation": "Rio Las Vegas Hotel",
+        "startMonth": "JAN",
+        "startDate": "16",
+        "startYear": "2026",
+        "guests": "Jensen Ackles, Jared Padalecki, Misha Collins, Rob Benedict, Richard Speight Jr., Felicia Day, Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"
+      },
+      {
+        "id": "boston3",
+        "location": "Boston, MA",
+        "eventLocation": "Westin Boston Seaport District Hotel",
+        "startMonth": "JUN",
+        "startDate": "12",
+        "startYear": "2026",
+        "guests": "Jensen Ackles, Jared Padalecki, Misha Collins, Rob Benedict, Richard Speight Jr., Kim Rhodes, and Briana Buckmaster"
+      },
+      {
+        "id": "toronto3",
+        "location": "Toronto, ON",
+        "eventLocation": "The Westin Harbour Castle Hotel",
+        "startMonth": "JUL",
+        "startDate": "10",
+        "startYear": "2026",
+        "guests": "Jensen Ackles, Jared Padalecki, Misha Collins, Rob Benedict, Richard Speight Jr., Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"
+      },
+       {
+        "id": "whippany3",
+        "location": "Whippany, NJ",
+        "eventLocation": "Hanover Marriott Hotel",
+        "startMonth": "AUG",
+        "startDate": "21",
+        "startYear": "2026",
+        "guests": "Jensen Ackles, Misha Collins, Rob Benedict, Richard Speight Jr., Felicia Day, Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"
+      },
+       {
+        "id": "orlando3",
+        "location": "Orlando, FL",
+        "eventLocation": "Doubletree by Hilton Orlando Hotel",
+        "startMonth": "SEPT",
+        "startDate": "11",
+        "startYear": "2026",
+        "guests": "Jensen Ackles, Misha Collins, Rob Benedict, Richard Speight Jr., Felicia Day, Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"
+      },
+      {
+        "id": "charlotte3",
+        "location": "Charlotte, NC",
+        "eventLocation": "Embassy Suites Charlotte-Concord Resort",
+        "startMonth": "SEPT",
+        "startDate": "25",
+        "startYear": "2026",
+        "guests": "Jensen Ackles, Misha Collins, Rob Benedict, Richard Speight Jr., Felicia Day, Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"
+      },
+      {
+        "id": "vancouver3",
+        "location": "Vancouver, BC",
+        "eventLocation": "Westin Bayshore Vancouver Hotel",
+        "startMonth": "OCT",
+        "startDate": "9",
+        "startYear": "2026",
+        "guests": "Jensen Ackles, Misha Collins, Rob Benedict, Richard Speight Jr., Felicia Day, Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"
+      },
+      {
+        "id": "columbus3",
+        "location": "Colombus, OH",
+        "eventLocation": "Greater Columbus Convention Center",
+        "startMonth": "OCT",
+        "startDate": "16",
+        "startYear": "2026",
+        "guests": "Jensen Ackles, Misha Collins, Rob Benedict, Richard Speight Jr., Felicia Day, Kim Rhodes, Briana Buckmaster, Jim Beaver, and David Haydn-Jones"
+      },
+      {
+        "id": "chicago3",
+        "location": "Chicago, IL",
+        "eventLocation": "Westin O'Hare Hotel",
+        "startMonth": "NOV",
+        "startDate": "6",
+        "startYear": "2026",
+        "guests": "Jensen Ackles, Misha Collins, Rob Benedict, Richard Speight Jr., Felicia Day, Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"
+      },
+      {
+        "id": "austin3",
+        "location": "Austin, TX",
+        "eventLocation": "Marriott Austin Downtown Hotel",
+        "startMonth": "NOV",
+        "startDate": "20",
+        "startYear": "2026",
+        "guests": "Jensen Ackles, Misha Collins, Rob Benedict, Richard Speight Jr., Felicia Day, Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"
+      }
+    ]
+  },
+  {
+    "id": "convention2",
+    "name": "The Road So Far... The Road Ahead On The High Seas",
+    "desc": "The Creation's SPN20 High Seas Cruise on the wondrous Royal Caribbean Cruise on the Wonder of the Seas Cruise Ship will cap the 20th Anniversary year of celebrating Supernatural in top-of-the-line luxury style and YOU are cordially invited!",
+    "imgsrc": "/assets/beyond/cruise.jpg",
+    "conventionList": [
+      {
+        "id": "cruise2",
+        "location": "Miami, Florida",
+        "eventLocation": "Port of Miami (Royal Caribbean Cruise Terminal)",
+        "startMonth": "NOV",
+        "startDate": "17",
+        "startYear": "2025",
+        "guests": "Misha Collins, Mark Sheppard, Ruth Connell, Rob Benedict, Richard Speight Jr., Kim Rhodes, Briana Buckmaster, DJ Qualls, Matt Cohen, Gil McKinney, and Clif Kosterman"
+      }
+    ]
+  },
     {
-        id:"convention1", name:"The Road So Far... The Road Ahead Creation Tour- Celebrating 20 Years of Supernatural", desc:"We are ready for 2025 with 10 incredible in-person engagements with superstars Jensen Ackles and Jared Padalecki. We celebrate 20 years of Supernatural with \"the boys\" and highlight their upcoming projects. Make your plans to be with us and check the calendar below for dates!", imgsrc:"/assets/beyond/20years.jpg",
-        conventionList:[
-            {id:"michigan", location:"Novi, Mi", eventLocation:"Suburban Collection Showplace", startMonth:"JULY", startDate:"11", startYear:"2025", guests:"Jensen Ackles, Misha Collins, Mark Sheppard, Alexander Calvert, Jim Beaver, Ruth Connell, Richard Speight Jr., Rob Benedict, Matt Cohen, Alaina Huffman, Kim Rhodes, Briana Buckmaster, Samantha Smith, Jake Abel, DJ Qualls, Gil McKinney, Chad Lindberg, Steven Williams, Jason Manns, Steve Carlson, and Louden Swain" },
-            {id:"texas", location:"Austin, Tx", eventLocation:"Marriott Austin Downtown Hotel", startMonth:"AUG", startDate:"22", startYear:"2025", guests:"Jensen Ackles, Jared Padalecki, Misha Collins, Mark Sheppard, Alexander Calvert, Jim Beaver, Ruth Connell, Richard Speight Jr., Rob Benedict, Matt Cohen, Alaina Huffman, Kim Rhodes, Briana Buckmaster, Samantha Smith, Jake Abel, DJ Qualls, Gil McKinney, Chad Lindberg, Jason Manns, and Louden Swain"},
-            {id:"chicago", location:"Chicago, Il", eventLocation:"Hyatt Regency O'Hare Hotel", startMonth:"SEPT", startDate:"5", startYear:"2025", guests:"Jensen Ackles, Jared Padalecki, Misha Collins, Mark Sheppard, Alexander Calvert, Jim Beaver, Ruth Connell, Rob Benedict, Richard Speight Jr., Matt Cohen, Alaina Huffman, Kim Rhodes, Briana Buckmaster, Samantha Smith, Felicia Day, Jake Abel, DJ Qualls, Gil McKinney, Jason Manns, and Louden Swain"},
-            {id:"vancouver", location:"Vancouver, BC", eventLocation:"Westin Bayshore Hotel", startMonth:"SEPT", startDate:"26", startYear:"2025", guests:"Jensen Ackles, Jared Padalecki, Misha Collins, Mark Sheppard, Alexander Calvert, Jim Beaver, Ruth Connell, Richard Speight Jr., Rob Benedict, Matt Cohen, Alaina Huffman, Kim Rhodes, Briana Buckmaster, Samantha Smith, Jake Abel, DJ Qualls, Gil McKinney, Jason Manns, and Louden Swain"},
-            {id:"philly", location:"Philadelphia, PA", eventLocation:"Sheraton Philadelphia Downtown Hotel", startMonth:"OCT", startDate:"3", startYear:"2025", guests:"Jensen Ackles, Jared Padalecki, Misha Collins , Mark Sheppard, Alexander Calvert, Jim Beaver, Felicia Day, Ruth Connell,  Rob Benedict, Richard Speight Jr., Matt Cohen, Alaina Huffman, Kim Rhodes, Briana Buckmaster, Samantha Smith, Jake Abel, Julie McNiven, DJ Qualls, Gil McKinney, Jeffrey Vincent Parise, Jason Manns, and Louden Swain"},
-            {id:"nashville", location:"Nashville, TN", eventLocation:"Grand Hyatt Nashville Hotel", startMonth:"OCT", startDate:"31", startYear:"2025", guests:"Jensen Ackles, Jared Padalecki, Misha Collins , Mark Sheppard, Alexander Calvert, Jim Beaver, Felicia Day, Ruth Connell,  Rob Benedict, Richard Speight Jr., Matt Cohen, Alaina Huffman, Kim Rhodes, Briana Buckmaster, Samantha Smith, Jake Abel, DJ Qualls, Gil McKinney, David Haydn-Jones, Jason Manns, and Louden Swain"},
-        ]
-    }, 
-    {
-        id:"convention2", name:"The Road So Far... The Road Ahead On The High Seas", desc:"The Creation's SPN20 High Seas Cruise on the wondrous Royal Caribbean Cruise on the Wonder of the Seas Cruise Ship will cap the 20th Anniversary year of celebrating Supernatural in top-of-the-line luxury style and YOU are cordially invited!",imgsrc:"/assets/beyond/cruise.jpg",
-        conventionList: [
-            {id:"cruise", location:"Miami, Florida", eventLocation:"Port of Miami (Royal Caribbean Cruise Terminal)", startMonth:"NOV", startDate:"17", startYear:"2025", guests:"Misha Collins, Mark Sheppard, Ruth Connell, Rob Benedict, Richard Speight Jr., Kim Rhodes, Briana Buckmaster, DJ Qualls, Matt Cohen, Gil McKinney, and Clif Kosterman"}
-        ]
-    },
-    {
-        id:"convention3", name:"The Road So Far... The Road Ahead. Creation Tour", desc:"We are ready for 2026 with several incredible in-person engagements with superstars Jensen Ackles and Jared Padalecki. We celebrate the adventures of two brothers who set of on a journey to save people and hunt things. Make your plans to be with us and check the calendar below for dates!",imgsrc:"/assets/beyond/rsf.png",
-        conventionList: [
-            {id:"vegas", location:"Las Vegas, NV", eventLocation:"Rio Las Vegas Hotel", startMonth:"JAN", startDate:"16", startYear:"2026", guests:"Jensen Ackles, Jared Padalecki, Misha Collins, Rob Benedict, Richard Speight Jr., Felicia Day, Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"},
-            {id:"boston", location:"Boston, MA", eventLocation:"Westin Boston Seaport District Hotel", startMonth:"JUN", startDate:"12", startYear:"2026", guests:"Jensen Ackles, Jared Padalecki, Misha Collins, Rob Benedict, Richard Speight Jr., Kim Rhodes, and Briana Buckmaster"},
-            {id:"toronto", location:"Toronto, ON", eventLocation:"The Westin Harbour Castle Hotel", startMonth:"JUL", startDate:"10", startYear:"2026", guests:"Jensen Ackles, Jared Padalecki, Misha Collins, Rob Benedict, Richard Speight Jr., Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"},
-            {id:"charlotte", location:"Charlotte, NC", eventLocation:"Embassy Suites Charlotte-Concord Resort", startMonth:"SEPT", startDate:"25", startYear:"2026", guests:"Jensen Ackles, Misha Collins, Rob Benedict, Richard Speight Jr., Felicia Day, Kim Rhodes, Briana Buckmaster, and David Haydn-Jones"},
-        ]
-    }
-    
+    "id": "convention1",
+    "name": "The Road So Far... The Road Ahead Creation Tour- Celebrating 20 Years of Supernatural",
+    "desc": "We are ready for 2025 with 10 incredible in-person engagements with superstars Jensen Ackles and Jared Padalecki. We celebrate 20 years of Supernatural with \"the boys\" and highlight their upcoming projects. Make your plans to be with us and check the calendar below for dates!",
+    "imgsrc": "/assets/beyond/20years.jpg",
+    "conventionList": [
+      {
+        "id": "michigan1",
+        "location": "Novi, Mi",
+        "eventLocation": "Suburban Collection Showplace",
+        "startMonth": "JULY",
+        "startDate": "11",
+        "startYear": "2025",
+        "guests": "Jensen Ackles, Misha Collins, Mark Sheppard, Alexander Calvert, Jim Beaver, Ruth Connell, Richard Speight Jr., Rob Benedict, Matt Cohen, Alaina Huffman, Kim Rhodes, Briana Buckmaster, Samantha Smith, Jake Abel, DJ Qualls, Gil McKinney, Chad Lindberg, Steven Williams, Jason Manns, Steve Carlson, and Louden Swain"
+      },
+      {
+        "id": "texas1",
+        "location": "Austin, Tx",
+        "eventLocation": "Marriott Austin Downtown Hotel",
+        "startMonth": "AUG",
+        "startDate": "22",
+        "startYear": "2025",
+        "guests": "Jensen Ackles, Jared Padalecki, Misha Collins, Mark Sheppard, Alexander Calvert, Jim Beaver, Ruth Connell, Richard Speight Jr., Rob Benedict, Matt Cohen, Alaina Huffman, Kim Rhodes, Briana Buckmaster, Samantha Smith, Jake Abel, DJ Qualls, Gil McKinney, Chad Lindberg, Jason Manns, and Louden Swain"
+      },
+      {
+        "id": "chicago1",
+        "location": "Chicago, Il",
+        "eventLocation": "Hyatt Regency O'Hare Hotel",
+        "startMonth": "SEPT",
+        "startDate": "5",
+        "startYear": "2025",
+        "guests": "Jensen Ackles, Jared Padalecki, Misha Collins, Mark Sheppard, Alexander Calvert, Jim Beaver, Ruth Connell, Rob Benedict, Richard Speight Jr., Matt Cohen, Alaina Huffman, Kim Rhodes, Briana Buckmaster, Samantha Smith, Felicia Day, Jake Abel, DJ Qualls, Gil McKinney, Jason Manns, and Louden Swain"
+      },
+      {
+        "id": "vancouver1",
+        "location": "Vancouver, BC",
+        "eventLocation": "Westin Bayshore Hotel",
+        "startMonth": "SEPT",
+        "startDate": "26",
+        "startYear": "2025",
+        "guests": "Jensen Ackles, Jared Padalecki, Misha Collins, Mark Sheppard, Alexander Calvert, Jim Beaver, Ruth Connell, Richard Speight Jr., Rob Benedict, Matt Cohen, Alaina Huffman, Kim Rhodes, Briana Buckmaster, Samantha Smith, Jake Abel, DJ Qualls, Gil McKinney, Jason Manns, and Louden Swain"
+      },
+      {
+        "id": "philly1",
+        "location": "Philadelphia, PA",
+        "eventLocation": "Sheraton Philadelphia Downtown Hotel",
+        "startMonth": "OCT",
+        "startDate": "3",
+        "startYear": "2025",
+        "guests": "Jensen Ackles, Jared Padalecki, Misha Collins , Mark Sheppard, Alexander Calvert, Jim Beaver, Felicia Day, Ruth Connell,  Rob Benedict, Richard Speight Jr., Matt Cohen, Alaina Huffman, Kim Rhodes, Briana Buckmaster, Samantha Smith, Jake Abel, Julie McNiven, DJ Qualls, Gil McKinney, Jeffrey Vincent Parise, Jason Manns, and Louden Swain"
+      },
+      {
+        "id": "nashville1",
+        "location": "Nashville, TN",
+        "eventLocation": "Grand Hyatt Nashville Hotel",
+        "startMonth": "OCT",
+        "startDate": "31",
+        "startYear": "2025",
+        "guests": "Jensen Ackles, Jared Padalecki, Misha Collins , Mark Sheppard, Alexander Calvert, Jim Beaver, Felicia Day, Ruth Connell,  Rob Benedict, Richard Speight Jr., Matt Cohen, Alaina Huffman, Kim Rhodes, Briana Buckmaster, Samantha Smith, Jake Abel, DJ Qualls, Gil McKinney, David Haydn-Jones, Jason Manns, and Louden Swain"
+      }
+    ]
+  },
 ];
